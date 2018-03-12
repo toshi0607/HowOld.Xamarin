2318d1475050fd6f8d47a3b3a611c5def3162956
toshi0607 <s.toshi0607@gmail.com>
2018-03-12 23:18:12 +0900

    :sparkles: add FaceDetection.cs

---
+++ HowOld.Xamarin/Face.Detection.cs
@@ -0,0 +1,13 @@
+﻿namespace HowOld.Xamarin
+{
+    public class FaceEmotionDetection
+    {
+        public string Emotion { get; set; }
+        public double Smile { get; set; }
+        public string Glasses { get; set; }
+        public string Gender { get; set; }
+        public double Age { get; set; }
+        public double Beard { get; set; }
+        public double Moustache { get; set; }
+    }
+}
