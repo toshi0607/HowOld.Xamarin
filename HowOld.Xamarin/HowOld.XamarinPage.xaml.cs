9e2ab9672c58621040fe2ad5ba4fd29017fb2121
toshi0607 <s.toshi0607@gmail.com>
2018-03-12 21:54:37 +0900

    first commit

---
+++ HowOld.Xamarin/HowOld.XamarinPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace HowOld.Xamarin
+{
+    public partial class HowOld_XamarinPage : ContentPage
+    {
+        public HowOld_XamarinPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
